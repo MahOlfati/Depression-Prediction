07d1574572743f04ea8b5052f3fe277210c0b0ee
MahOlfati <97849098+MahOlfati@users.noreply.github.com>
2024-01-31 11:47:30 +0330

    Update Sleep_predictor.m

--- Sleep_predictor.m
+++ Sleep_predictor.m
@@ -47,7 +47,6 @@ x = sleep;             % Put predictors here
 y = depression;        % Put target here
 
 %% Nested 10-fold cross-validation considering the family structure
-
 [test_idx,train_outer_idx,train_inner_idx,validation_idx] = NestedCV(y,family)
 
 %% Training models, saving the best models, and fitting selected models on outer folds
@@ -64,12 +63,10 @@ for h = 1:kf           % Outer folds
     conftrain_outer = confounding(train_outer_idx{h},:);
     conftest = confounding(test_idx{h},:);
    
-    % creating confound removal models (conf_mdl) on training sets and applying them on test sets
-    
+    % creation of confound removal models (conf_mdl) on training sets and applying them on test sets
     [x_train,x_test,conf_mdl] = Confound_Remove_model(h,x_train,x_test,conftrain_outer,conftest);
     
     % Ranking features in outer training folds
-    
     rng default
     [ranks{h},weights{h}] = relieff(x_train,y_train,10);
 
@@ -83,21 +80,21 @@ for h = 1:kf           % Outer folds
         conftrain_inner = confounding(train_inner_idx{h,i},:);
         conf_validation = confounding(validation_idx{h,i},:);
         
-        % regressing out the cnfound variables from inner-training/validation predictors
+        % regressing out the cnfound variables from inner training and validation predictors
         [x_trainn,x_validation] = RegressOut(h,x_trainn,conftrain_inner,x_validation,conf_validation,conf_mdl,ranks,feature_num)
         
         % Training models on inner training folds
         Mdl1= Model(i,x_trainn,y_trainn)
         
-        % Assessment of error on validation sets
+        % Assessment of loss-function of validation sets to select model with best performance
         ls{h,i} = loss(Mdl1{i},x_validation,y_validation);
     end
 
-    % Selection of best model for each outer fold
+    % Selection of best model for each outer fold based on minimum loss of ineer folds
     [lost{h},best{h}] = min(cell2mat(ls(h,:)));
     bestmodel{h} = Mdl1{best{h}};
     
-    % fitting selected model on entire training sets
+    % fitting selected model on the entire training sets and prediction of test sets
     rng default
     tmp{h} = classreg.learning.FitTemplate.makeFromModelParams(bestmodel{h}.ModelParameters);
     newmodel{h} = fit(tmp{h},x_train(:,ranks{h}),y_train);
@@ -106,10 +103,11 @@ for h = 1:kf           % Outer folds
      
 end
 
-%% Plotting
+%% Predicted values
 YHat(cell2mat(test_idx')) = cell2mat(Yhat1');
 YHat = YHat';
 
+%% Plotting
 reg = [YHat,y];
 figure;
 [R,PValue] = corrplot(reg,'testR','on')
@@ -127,5 +125,4 @@ ci = mean(YHat)+ts*sem
 MSE = mean(cell2mat(ls1))
 
 %% Storage of models for out of sample validation
-
 save('modelanxi_sleep_conf.mat','newmodel','ranks','conf_mdl','reg')
