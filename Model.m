8f61bdb0bf038ef41170b754fb69d72fb789d47a
Mahnaz Olfati <97849098+Mahnaz-Olfati@users.noreply.github.com>
2024-07-30 17:30:13 +0330

    Delete Model.m

--- Model.m
+++
@@ -1,6 +0,0 @@
-function Mdl1 = Model(x_trainn,y_trainn)
-rng default
-        Mdl1 = fitrensemble(x_trainn,y_trainn,'OptimizeHyperparameters','auto','Resample','on',...
-            'HyperparameterOptimizationOptions',struct('Optimizer','bayesopt','ShowPlots',false,'UseParallel',false,'AcquisitionFunctionName',...
-            'expected-improvement-plus','MaxObjectiveEvaluations',1,'Repartition',true));
-return
