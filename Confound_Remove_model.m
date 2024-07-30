cea21e20a2e2cd505052f97952c098b13c7e051f
Mahnaz Olfati <97849098+Mahnaz-Olfati@users.noreply.github.com>
2024-07-30 17:38:44 +0330

    Add files via upload

---
+++ Confound_Remove_model.m
@@ -0,0 +1,9 @@
+function [x_train,x_test,conf_mdl] = Confound_Remove_model(h,x_train,x_test,conftrain_outer,conftest)
+
+    for culm = 1:size(x_train,2)
+            conf_mdl{h,culm} = fitlm(conftrain_outer, x_train(:,culm));
+            x_train(:,culm) = table2array(conf_mdl{h,culm}.Residuals(:,1));
+            x_test(:,culm) = x_test(:,culm) - predict(conf_mdl{h,culm},conftest);
+    end
+    
+end
