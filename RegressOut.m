aadc29500f6553be79a95cec3c725b6faf11fd8a
Mahnaz Olfati <97849098+Mahnaz-Olfati@users.noreply.github.com>
2024-07-31 18:28:43 +0330

    Delete RegressOut.m

--- RegressOut.m
+++
@@ -1,8 +0,0 @@
-function [x_trainn,x_validation] = RegressOut(h,x_trainn,conftrain_inner,x_validation,conf_validation,conf_mdl,ranks,feature_num)
-
-    for culmn = 1:feature_num
-            x_trainn(:,culmn) = x_trainn(:,culmn) - predict(conf_mdl{h,ranks{h}(culmn)},conftrain_inner);
-            x_validation(:,culmn) = x_validation(:,culmn) - predict(conf_mdl{h,ranks{h}(culmn)},conf_validation);
-    end
-    
-end
